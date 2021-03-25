e4b90e8b872b1433d39ac4f05e44b961e19fed4b
Mridul Seth <seth.mridul@gmail.com>
2021-03-25 15:21:12 +0100

    update env

--- reproduce.sh
+++ reproduce.sh
@@ -1,7 +1,7 @@
 # requirements setup and mamba for faster install for env
 source /opt/conda/etc/profile.d/conda.sh
 mamba env create -qq -f environment.yml
-conda activate krusellsmith
+conda activate solvingmicrodsops
 
 # execute script to reproduce figures
 cd Code/
