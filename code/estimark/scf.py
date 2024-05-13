4aedb8f90ddbcff075c375d3a1b9f4ac0d33ea7a
alanlujan91 <alanlujan91@gmail.com>
2024-05-13 16:03:26 -0400

    update tables

--- code/estimark/scf.py
+++ code/estimark/scf.py
@@ -15,15 +15,16 @@ from estimark.parameters import (
 csv_file_path = Path(__file__).resolve().parent / ".." / "data" / "SCFdata.csv"
 
 # Define the variables to keep
-keep_vars = ["age", "age_group", "wealth_income_ratio", "weight"]
+keep_vars = ["age", "age_group", "wealth_income_ratio", "weight", "wave"]
 
 # Read the CSV file and filter data in one step
 scf_data = pd.read_csv(csv_file_path)
-scf_data = scf_data.loc[
+scf_data_full = scf_data.loc[
     (scf_data.norminc > 0.0)
     & (scf_data.education == education)
     & (scf_data.age > initial_age)
-    & (scf_data.age <= final_age_data)
-    & (~scf_data.age.isin(remove_ages_from_scf)),
+    & (scf_data.age <= final_age_data),
     keep_vars,
 ]
+
+scf_data = scf_data_full.loc[~scf_data.age.isin(remove_ages_from_scf)]
